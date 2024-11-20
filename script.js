5624ad9aa53d297ab3a4239644384d1814f24ef6
Basant-spec <basantdhiman9@gmail.com>
2024-11-20 13:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ const inputs = document.querySelectorAll('.code');
 // Add event listeners to each input
 inputs.forEach((input, index) => {
   input.addEventListener('input', function () {
-    // If a user types a digit, move to the next input
-    if (input.value && index < inputs.length - 1) {
-      inputs[index + 1].focus();
+    // Ensure only digits are allowed
+    if (/^[0-9]$/.test(input.value)) {
+      // If valid digit and not the last input, move focus to the next input
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    } else {
+      // Clear invalid input
+      input.value = '';
     }
   });
 
   input.addEventListener('keydown', function (e) {
-    // If Backspace is pressed and the input is empty, move to the previous input
+    // Handle Backspace
     if (e.key === 'Backspace') {
+      // If input is empty, move to the previous input
       if (!input.value && index > 0) {
         inputs[index - 1].focus();
       }
