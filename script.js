56a06a56d7468120a3d0d05146597782b4d514bc
Basant-spec <basantdhiman9@gmail.com>
2024-11-20 13:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
+// Select all the input fields with the class "code"
+const inputs = document.querySelectorAll('.code');
+
+// Add event listeners to each input
 inputs.forEach((input, index) => {
   input.addEventListener('input', function () {
+    // If a user types a digit, move to the next input
     if (input.value && index < inputs.length - 1) {
       inputs[index + 1].focus();
-      updateFocusState(index + 1);
     }
   });
 
   input.addEventListener('keydown', function (e) {
-    if (e.key === 'Backspace' && !input.value && index > 0) {
+    // If Backspace is pressed and the input is empty, move to the previous input
+    if (e.key === 'Backspace') {
+      if (!input.value && index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+
+    // Prevent entering anything other than digits
+    if (!/[0-9]/.test(e.key) && e.key !== 'Backspace' && e.key !== 'ArrowLeft' && e.key !== 'ArrowRight') {
+      e.preventDefault();
+    }
+  });
+
+  // Handle arrow navigation
+  input.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft' && index > 0) {
       inputs[index - 1].focus();
-      updateFocusState(index - 1);
+    } else if (e.key === 'ArrowRight' && index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
   });
 });
-
-// Function to manage the `focused` class
-function updateFocusState(index) {
-  inputs.forEach((el, i) => el.classList.toggle('focused', i === index));
-}
